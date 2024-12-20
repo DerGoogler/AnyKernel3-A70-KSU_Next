aca8d2f88e94b83dc6e9a2946ecae99cfb995bc0
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-12-20 19:04:42 +0100

    .

--- anykernel.sh
+++ anykernel.sh
@@ -17,14 +17,6 @@ supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
-
-### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-} # end attributes
-
 # boot shell variables
 BLOCK=boot;
 IS_SLOT_DEVICE=0;
